3fe507b245e793fbe1f46b75634eff31ed22dbee
Benjamin Hale <apollo@arasaia.net>
2014-05-16 16:35:38 -0500

    chapter 3 incomplete; do graham scan later

--- ch03/exercises.hs
+++ ch03/exercises.hs
@@ -58,7 +58,7 @@ treeHeight (Node _ left right) = 1 + max (treeHeight left) (treeHeight right)
 
 -- 9
 data Direction = CW | CCW | COL
-    deriving (Show)
+    deriving (Show, Eq)
 
 -- 10
 type Point2D = (Double, Double)
@@ -76,7 +76,57 @@ directions (a:b:[]) = []
 directions [] = []
 
 -- 12
---graham (x:xs)
+-- some bits from:
+-- http://www.fatvat.co.uk/2009/09/graham-scan-in-haskell.html
+sortCoord :: Point2D -> Point2D -> Ordering
+sortCoord p1 p2
+    | y1 == y2 = compare x1 x2
+    | snd p1 < snd p2 = LT
+    | otherwise = GT
+
+sortPoints :: [Point2D] -> [Point2D]
+sortPoints xs = sortBy sortCoord xs
+
+
+--list of points?
+testPoints1 = [(2.282154, 1.107697),
+    (0.508136, 3.557593),
+    (6.490489, 7.220862),
+    (5.965358, 7.999035),
+    (8.800029, 5.776057),
+    (8.404778, 6.139152),
+    (0.743481, 2.498024),
+    (1.002515, 1.441651),
+    (6.814331, 4.425290),
+    (0.351636, 7.519613),
+    (7.209313, 4.625614),
+    (3.246308, 8.286591),
+    (1.427197, 8.788250),
+    (8.295495, 1.439363),
+    (6.410940, 2.408661),
+    (4.719379, 4.218519),
+    (5.873703, 6.961398),
+    (6.572547, 5.621853),
+    (1.308362, 2.677804),
+    (3.789837, 3.064397)]
+
+{-- correct hull?
+ [(2.282154, 1.107697),
+(8.295495, 1.439363),
+(8.800029, 5.776057),
+(8.404778, 6.139152),
+(5.965358, 7.999035),
+(1.427197, 8.78825),
+(0.351636, 7.519613),
+(0.508136, 3.557593),
+(0.743481, 2.498024),
+(1.002515, 1.441651)]
+--}
+
+
+
+
+
 
 
 
